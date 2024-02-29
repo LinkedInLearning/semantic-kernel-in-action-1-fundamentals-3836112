5437762ecf5f6746075f2a4264aa9a6aeeb7412a
Jose Luis Latorre Millas <joslat@gmail.com>
2024-02-29 21:08:38 +0100

    practice improvement

--- src/03_03e/PromptFunction.cs
+++ src/03_03e/PromptFunction.cs
@@ -4,6 +4,8 @@ namespace _03_03e;
 
 public class PromptFunction
 {
+  private static string someText = "Effective prompt design is essential to achieving desired outcomes with LLM AI models. Prompt engineering, also known as prompt design, is an emerging field that requires creativity and attention to detail. It involves selecting the right words, phrases, symbols, and formats that guide the model in generating high-quality and relevant texts.\r\n\r\nIf you've already experimented with ChatGPT, you can see how the model's behavior changes dramatically based on the inputs you provide. For example, the following prompts produce very different outputs:";
+
   public static async Task Execute()
   {
     var modelDeploymentName = "Gpt4v32k";
@@ -19,21 +21,39 @@ public class PromptFunction
     );
     var kernel = builder.Build();
 
+    await CreateAndExecuteAPrompt(kernel);
+    await ImportPluginFromFolderAndExecuteIt(kernel);
+
+    Console.ReadLine();
+  }
+
+  public static async Task CreateAndExecuteAPrompt(Kernel kernel)
+  {
+    var prompt = "What is the meaning of life?";
+    var kernelFunction = kernel.CreateFunctionFromPrompt(prompt);
+    var result = await kernelFunction.InvokeAsync(kernel);
+
+    Console.WriteLine(result);
+  }
+
+  public static async Task ImportPluginFromFolderAndExecuteIt(Kernel kernel)
+  {
+    // Import a plugin from a prompt directory
     var SummarizePluginDirectory = Path.Combine(
         System.IO.Directory.GetCurrentDirectory(),
         "plugins",
         "SummarizePlugin");
     kernel.ImportPluginFromPromptDirectory(SummarizePluginDirectory);
 
-    string someText = "Effective prompt design is essential to achieving desired outcomes with LLM AI models. Prompt engineering, also known as prompt design, is an emerging field that requires creativity and attention to detail. It involves selecting the right words, phrases, symbols, and formats that guide the model in generating high-quality and relevant texts.\r\n\r\nIf you've already experimented with ChatGPT, you can see how the model's behavior changes dramatically based on the inputs you provide. For example, the following prompts produce very different outputs:";
-
     var summarizeResult =
-        await kernel.InvokeAsync("SummarizePlugin", "Summarize", new() {
-                    { "input", someText }
-        });
-
+        await kernel.InvokeAsync(
+          "SummarizePlugin",
+          "Summarize",
+          new() {
+            { "input", someText }
+          });
 
     Console.WriteLine($"Result:  {summarizeResult}");
-    Console.ReadLine();
   }
+
 }
